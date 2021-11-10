ca11a61168031ad94c1be9207091c7fb088a1a87
Ali Sheerzad <ali.sheerzad9@gmail.com>
2021-11-10 17:10:21 +0200

    added functions

--- main.js
+++ main.js
@@ -1,79 +1,92 @@
 const addBtn = document.querySelector(".addBtn");
 const inputText = document.querySelector(".inputSearch");
 const nationContainer = document.querySelector(".nationContainer");
+const neighboursContainer = document.querySelector(".neighboursContainer")
 
-function renderCountry(data, className = "") {
+let borders = []
+
+function renderCountry(data, language, currency) {
   const html = `
-    <article class = "country ${className}">
+    <article class = "country">
         <img class="country__img" src="${data.flags.png}" />
         <div class="country__data">
-            <h4>${data.name}</h4>
+            <h4>${data.name.common}</h4>
             <h3>${data.region}</h3>
             <p><span>👨‍👩‍👦‍👦</span> Population: ${(
               +data.population / 1000000
             ).toFixed(1)} million</p>
-            <p><span>🗣</span> Language: ${data.languages[0].name} </p>
-            <p><span>💰</span> Currency: ${data.currencies[0].name}</p>
+            <p><span>🗣</span> Language: ${language} </p>
+            <p><span>💰</span> Currency: ${currency}</p>
         </div>
     </article>`;
-  nationContainer.insertAdjacentHTML("afterbegin", html);
+  nationContainer.insertAdjacentHTML("beforeend", html);
+}
+
+function renderNeighbours(data, currency, language){
+  console.log();
+  const html = `
+    <article class = "neighbourCountry">
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+            <h4>${data.name.common}</h4>
+            <h3>${data.region}</h3>
+            <p><span>👨‍👩‍👦‍👦</span> Population: ${(
+              +data.population / 1000000
+            ).toFixed(1)} million</p>
+            <p><span>🗣</span> Language: ${language} </p>
+            <p><span>💰</span> Currency: ${currency}</p>
+        </div>
+    </article><br><br>`;
+  neighboursContainer.insertAdjacentHTML("beforeend", html);
+
 }
 
 function getCountry(country) {
-  fetch(`https://restcountries.com/v2/name/${country}?fullText=true`)
+  fetch(`https://restcountries.com/v3.1/name/${country}?fullText=true`)
     .then((res) => {
       const data = res.json();
       return data;
     })
     .then((data) => {
+      const language = Object.values(data[0].languages)[0]
+      const currency = Object.values(data[0].currencies)[0].name
       console.log(data[0]);
-      renderCountry(data[0]);
+      borders = data[0].borders
+      console.log(borders);
+      renderCountry(data[0], language, currency);
+      return borders
+    }).then((borders) => {
+      borders.forEach(el => threeDigitcountry(el))
     });
 }
 
 addBtn.addEventListener("click", function () {
-  console.log(inputText.value);
   getCountry(inputText.value);
 });
 
-function testData(country) {
-  fetch(`https://restcountries.com/v2/name/${country}?fullText=true`)
-    .then((res) => {
-      const data = res.json();
-      return data;
-    })
-    .then((data) => {
-      console.log(data[0]);
-      console.log(data[0].borders);
-    });
-}
 
 function threeDigitcountry(country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
+  fetch(`https://restcountries.com/v3.1/alpha/${country}`)
     .then((res) => {
       const data = res.json();
       return data;
     })
     .then((data) => {
+      const currency = Object.values(data[0].currencies)[0].name
+      const language = Object.values(data[0].languages)[0]
       console.log(data[0]);
-      // renderCountry(data[0]);
+      renderNeighbours(data[0], currency, language);
     });
 }
-threeDigitcountry("swe");
-testData("finland");
+// threeDigitcountry("nor");
 
-//getCountry("finland");
 
-// fetch("https://restcountries.com/v2/name/finland?fullText=true")
-//   .then((res) => {
-//     const data = res.json();
-//     return data;
+// function apiTest(){
+//   fetch(`https://goweather.herokuapp.com/weather/helsinki`).then((res) => {
+//     const data = res.json()
+//     return data
+//   }).then((data) => {
+//     console.log(data);
 //   })
-//   .then((data) => console.log(data[0]));
-
-// fetch("https://restcountries.com/v3.1/name/portugal?fullText=true")
-//   .then((res) => {
-//     const data = res.json();
-//     return data;
-//   })
-//   .then((data) => console.log(data[0].languages));
+// }
+// apiTest()
